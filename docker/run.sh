9f6ef378393d500e95e17d08f2ff5fb573cfd3bf
lingjunliu <rienshaliu@gmail.com>
2024-04-24 15:21:07 +0900

    chore: edit .gitignore to add .env file in repository

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,7 @@
 cd ..
 
 cp -r init.sql ./docker/db
-cp -r .env requirements.txt ./server ./docker/backend
+cp -r requirements.txt ./server ./docker/backend
 cp -r ./assets ./public ./src .eslintrc.json next.config.js package-lock.json package.json tsconfig.json ./docker/frontend
 
 cd docker

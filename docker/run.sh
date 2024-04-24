b7a70de47822d5302fa563409f17a5a7bfc6a42a
lingjunliu <rienshaliu@gmail.com>
2024-04-24 15:02:41 +0900

    feat: add script to run docker-compose automatically
    - copy files to folder db, backend, and frontend
    - run docker-compose

---
+++ docker/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ..
+
+cp -r init.sql ./docker/db
+cp -r .env requirements.txt ./server ./docker/backend
+cp -r ./assets ./public ./src .eslintrc.json next.config.js package-lock.json package.json tsconfig.json ./docker/frontend
+
+cd docker
+
+docker-compose up --build
